771da40164cd2bb2c7fbf548d74d3411c45e2341
Aoy Natthaphong <nath.wangsungnone@gmail.com>
2025-01-16 13:34:19 +0700

    update

--- resources/views/home/index.blade.php
+++ resources/views/home/index.blade.php
@@ -2,19 +2,19 @@
 @extends('layouts.app')
 
 @section('content')
-    @include('home.components.page-1')
-    @include('home.components.page-2')
-    @include('home.components.page-3')
-    @include('home.components.page-4')
-    @include('home.components.page-5')
-    @include('home.components.page-6')
-    @include('home.components.page-7')
-    @include('home.components.page-8', [
-        'procurement' => $procurement,
-        'procurementResults' => $procurementResults,
-        'average' => $average,
-        'revenue' => $revenue,
-    ])
-    @include('home.components.page-9')
-    @include('home.components.page-10')
+@include('home.components.page-1')
+@include('home.components.page-2')
+@include('home.components.page-3')
+@include('home.components.page-4')
+@include('home.components.page-5')
+@include('home.components.page-6')
+@include('home.components.page-7')
+@include('home.components.page-8', [
+'procurement' => $procurement,
+'procurementResults' => $procurementResults,
+'average' => $average,
+'revenue' => $revenue,
+])
+@include('home.components.page-9')
+@include('home.components.page-10')
 @endsection
